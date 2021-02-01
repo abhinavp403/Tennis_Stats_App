5c1d3f41ee6a8c81b64f2542e16b673e16a37e25
Abhinav <abhinavp403@gmail.com>
2021-01-31 19:50:47 -0600

    Seventh Commit

--- app/src/main/python/playerExist.py
+++ app/src/main/python/playerExist.py
@@ -4,26 +4,16 @@ from os.path import dirname, join
 def isPlayerExistMale(name):
     with open(join(dirname(__file__), 'atp_players.csv'), mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        name = name.split(" ")
-        if len(name) == 1:
-            return False
-        first = name[0]
-        last = name[1]
         for row in csv_reader:
-            if first == row["firstname"] and last == row["lastname"]:
-                return True
+        if name == row["firstname"] + " " + row["lastname"]:
+            return True
     return False
 
 
 def isPlayerExistFemale(name):
     with open(join(dirname(__file__), 'wta_players.csv'), mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        name = name.split(" ")
-        if len(name) == 1:
-            return False
-        first = name[0]
-        last = name[1]
         for row in csv_reader:
-            if first == row["firstname"] and last == row["lastname"]:
+            if name == row["firstname"] + " " + row["lastname"]:
                 return True
     return False
